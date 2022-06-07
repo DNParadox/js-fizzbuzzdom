5620e54065f31a2d5094455764048bbf1d07641b
DNParadox <m96francesco@gmail.com>
2022-06-07 16:26:06 +0200

    push!

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,9 +1,10 @@
 for(let i = 0; i <= 10; i++) {
-
+ 
     if( i % 3 === 0) {
-        i = 'fizz'
-    } else (i % 5 === 0)
-        i = 'buzz'
+        i = 'fizz'; }
+    // } if (i % 5 === 0) {
+    //     i = 'buzz';
+    // // }
     console.log(i)
  
   }
