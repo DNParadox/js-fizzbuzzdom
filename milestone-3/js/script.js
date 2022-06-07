e2cfdd15f6ce6406c00c6e69f690babf79331101
DNParadox <m96francesco@gmail.com>
2022-06-07 19:28:46 +0200

    Milestone3

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -1,14 +1,14 @@
 const numbersContainer = document.querySelector('.inner');
 
 for(let i = 1; i <= 100; i++) {
-    let newLi = `<li>${i} </li> `;
+    let newLi = `<div class="box">${i} </div> `;
 
     if( i % 3 === 0 && i % 5 === 0 ) {
-        numbersContainer.append('fizzbuzz');
+        numbersContainer.innerHTML +='<div class="box red">Buzzfizz</div>';
     } else if ( i % 3 === 0) {
-        numbersContainer.append ('fizz');
+        numbersContainer.innerHTML +='<div class="box green">Fizz</div>';
     } else if (i % 5 === 0) {
-        numbersContainer.append ('buzz');
+        numbersContainer.innerHTML +='<div class="box yellow">Buzz</div>';
     
     }else 
     numbersContainer.innerHTML += newLi;
