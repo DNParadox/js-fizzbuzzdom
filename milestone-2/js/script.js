ef0e203cdc16c36e60a5e6c2a9413d5f41a0c416
DNParadox <m96francesco@gmail.com>
2022-06-08 14:55:59 +0200

    Repush!

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -4,7 +4,7 @@ for(let i = 1; i <= 100; i++) {
     let newLi = `<li>${i} </li> `;
 
     if( i % 3 === 0 && i % 5 === 0 ) {
-        numbersContainer.append += '<li>fizzbuzz</li>';
+        numbersContainer.innerHTML += '<li>fizzbuzz</li>';
     } else if ( i % 3 === 0) {
         numbersContainer.innerHTML +='<li>fizz</li>';
         console.log(newLi)
